fd2c6e1055997c21404f19ae60cd3bc14af9adb9
MonteiroCadu <cadumonteiro.setmidia@gmail.com>
2022-05-31 18:42:17 -0300

    Refatoração do modal de confirmação de exclução componentizado em um partial view

--- Src/Front/SisCadProdSelecao.Web/wwwroot/js/site.js
+++ Src/Front/SisCadProdSelecao.Web/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function setDelete(id) {
+
+    idParaDeletar = id;
+
+};
+
+function deletaProduto(uri) {    
+    window.location.replace(uri + idParaDeletar);
+}
